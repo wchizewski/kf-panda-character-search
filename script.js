e9886de73ea7d38251de6a871d41b48853ecd68f
wchizewski <w.chizewski@share.epsb.ca>
2021-09-21 13:45:53 -0600

    jo

--- script.js
+++ script.js
@@ -13,30 +13,32 @@ searchBtnEl.addEventListener("click", characterSearch)
 // Event Function
 function characterSearch() {
     // Get input
-    let name = nameInputEl.value;
+    let name = nameInputEl.value.toLowerCase();
+   
+   
 
 // Check the name
-if (name === "Po") {
+if (name === "po" || name === "dragon warrior" || name === "the dragon warrior") {
     charImgEl.src = "images/po.png";
     charNameEl.innerHTML = "Po";
     charQuoteEl.innerHTML = "\"Buddy, I am the Dragon Warrior.\"";
-} else if (name === "Viper") {
+} else if (name === "viper") {
     charImgEl.src = "images/viper.png";
     charNameEl.innerHTML = "Viper";
     charQuoteEl.innerHTML = "\"I don't need to bite to fight!\"";
-} else if (name === "Mantis") {
+} else if (name === "mantis") {
     document.getElementById("char-img").src = "images/mantis.png";
     charNameEl.innerHTML = "Mantis";
     charQuoteEl.innerHTML = "\"Fear the bug!\"";
-} else if (name === "Crane") {
+} else if (name === "crane") {
     document.getElementById("char-img").src = "images/crane.png";
     charNameEl.innerHTML = "Crane";
     charQuoteEl.innerHTML = "\"You can chain my body, but you will never chain my warrior spirit!\"";
-} else if (name === "Tigress") {
+} else if (name === "tigress") {
     document.getElementById("char-img").src = "images/tigress.png";
     charNameEl.innerHTML = "Tigress";
     charQuoteEl.innerHTML = "\"That was pretty hardcore!\"";
-} else if (name === "Monkey") {
+} else if (name === "monkey") {
     document.getElementById("char-img").src = "images/monkey.png";
     charNameEl.innerHTML = "Monkey";
     charQuoteEl.innerHTML = "\"We should hang out!\"";
